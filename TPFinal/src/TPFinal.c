316e7c5e29c73ce154d3f0147b44b6c0ac4fee84
santicolquev <santiagocolquev@gmail.com>
2023-11-02 18:21:33 -0300

    .

--- TPFinal/src/TPFinal.c
+++ TPFinal/src/TPFinal.c
@@ -9,7 +9,39 @@
 #include <cr_section_macros.h>
 
 
-int main(void) {
-    while(1) {}
-    return 0;
+void pin_config(){
+
 }
+
+void adc_config(){
+
+}
+
+void dac_config(){
+
+}
+
+void timer_config(){
+
+}
+
+void dma_config(){
+
+}
+
+void uart_config(){
+
+}
+
+int main(void) {
+    pin_config();
+    adc_config();
+    dac_config();
+    timer_config();
+    dma_config();
+    while(1) {
+        //TODO
+    }
+    return 0 ;
+}
+
